1e506c1608bf8cae69f66c822f5bca255f0f91fe
Mikhail Hyde <mikhail.hyde@formlabs.com>
2023-09-16 22:10:24 -0400

    add goto-preview shortcuts

--- files/config/nvim/lua/user/plugins/goto.lua
+++ files/config/nvim/lua/user/plugins/goto.lua
@@ -21,5 +21,10 @@ return {
 			stack_floating_preview_windows = true, -- Whether to nest floating windows
 			preview_window_title = { enable = true, position = "left" }, -- Whether 
 		}
+    vim.keymap.set("n", "gpd", "<cmd>lua require('goto-preview').goto_preview_definition()<CR>")
+    vim.keymap.set("n", "gpt", "<cmd>lua require('goto-preview').goto_preview_type_definition()<CR>")
+    vim.keymap.set("n", "gpi", "<cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
+    vim.keymap.set("n", "gP", "<cmd>lua require('goto-preview').close_all_win()<CR>")
+    vim.keymap.set("n", "gpr", "<cmd>lua require('goto-preview').goto_preview_references()<CR>")
 	end
 }
