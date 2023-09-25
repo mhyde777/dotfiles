0252827042e07e8972dde018afb4b90699017d97
Mikhail Hyde <mikhail.hyde@formlabs.com>
2023-09-24 23:00:31 -0400

    snap script works

--- scripts/rm_snap.sh
+++ scripts/rm_snap.sh
@@ -4,23 +4,30 @@ if command -v lsb_release >/dev/null 2>&1 ; then
     os_name=$(lsb_release -is)
 
     if [ "$os_name" = "Ubuntu" ]; then
-        sudo umount /snap/core
+        echo "OS is Ubuntu. Continuing..."
         if command -v snap >/dev/null 2>&1 ; then
+            echo "Snap is installed. Continuing..."
+            sudo umount /snap/core/*
             snap list | awk 'NR>1 {print $1}' | while read -r package_name; do
-                echo "Removing $package_name"
+                echo "removing $package_name"
                 sudo snap remove --purge $package_name 
             done
+            # A hack to circle back and get packages like snapd, core, etc
+            snap list | awk 'NR>1 {print $1}' | while read -r package_name; do
+                echo "removing $package_name"
+                sudo snap remove --purge $package_name 
+            done
+            sudo apt autoremove snapd -y
         else
           echo "snap command not found. Exiting."
-          exit 1
         fi
-        sudo apt autoremove snapd
         sudo touch /etc/apt/preferences.d/nosnap.pref
-        sudo echo "Package: snapd" > /etc/apt/preferences.d/nosnap.pref
+        sudo echo "Package: snapd" >> /etc/apt/preferences.d/nosnap.pref
         sudo echo "Pin: release a=*" >> /etc/apt/preferences.d/nosnap.pref
         sudo echo "Pin-Priority: -10" >> /etc/apt/preferences.d/nosnap.pref
         sudo apt install --install-suggests gnome-software
-        sudo apt rm -rf $HOME/snap
+        sudo rm -rf $HOME/snap
+        sudo apt update
     else
         echo "This script is intended to run on Ubuntu. Exiting."
         exit 1
