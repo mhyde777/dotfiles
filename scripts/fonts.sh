a79da4bae2bd39c14cf2928417dce66402468288
Mikhail Hyde <mikhail.hyde@formlabs.com>
2023-09-24 23:06:37 -0400

    fonts work

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,15 +1,25 @@
 #! /bin/bash
 
-DIR=$HOME/.local/share/fonts
+# Create fonts directory
 
+DIR=$HOME/.local/share/fonts
 if [ -d "$DIR"]; then
     echo "Directory exists. Continuing..."
 else
     mkdir "$DIR"
 fi
 
+# Download Extract
 cd $HOME/Downloads
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraMono.zip
-unzip FiraMono.zip $HOME/.local/share/fonts/
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.zip ~/Downloads/
+unzip FiraMono.zip
+
+# Copy all font files to created DIR
+cp *.otf $DIR
+
+# Load fonts
 fc-cache -fv
+
+# Clean Downlaods
+rm $HOME/Downloads/*
 
