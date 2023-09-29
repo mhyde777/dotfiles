9d6bb9ae58dd56a8650fc8503c8cf7540681f5ad
Mikhail Hyde <mikhail.hyde@formlabs.com>
2023-09-28 23:22:04 -0400

    default harpoon width

--- files/config/nvim/lua/user/plugins/harpoon.lua
+++ files/config/nvim/lua/user/plugins/harpoon.lua
@@ -2,6 +2,11 @@ return {
 	'ThePrimeagen/harpoon',
 	config = function()
 		require("telescope").load_extension('harpoon')
+    require("harpoon").setup({
+      menu = {
+        width = vim.api.nvim_win_get_width(0) - 50,
+      }
+    })
 		vim.keymap.set('n', '<leader>sm', ":Telescope harpoon marks<CR>", { desc = 'Harpoon [M]arks' })
 		vim.api.nvim_set_keymap("n", "<leader>m", ":lua require('harpoon.mark').add_file()<CR>", {noremap=true})
 		vim.api.nvim_set_keymap("n", "<leader>hh", ":lua require('harpoon.ui').toggle_quick_menu()<CR>", {noremap=true})
