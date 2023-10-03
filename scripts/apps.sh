fddbdceb9e29abaf4903f38b6888ae837468edbd
Mikhail Hyde <hyde.mikhail@gmail.com>
2023-10-02 20:02:48 -0400

    cleaning house

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -4,29 +4,30 @@ yes | sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt update
 
 # Software Essentials
-sudo apt install               \
-    cmake                      \
-    make                       \
-    build-essential            \
-    software-properties-common \
-    python3-dev                \
-    libstdc++-13-dev           \
-    pkg-config                 \
-    gcc-13                     \
+sudo apt install                \
+    cmake                       \
+    make                        \
+    build-essential             \
+    software-properties-common  \
+    python3-dev                 \
+    libstdc++-13-dev            \
+    pkg-config                  \
+    gcc-13                      \
     g++-13 -y
 
 # Applications Only I Would Want
-sudo apt install               \
-    curl                       \
-    htop                       \
-    keychain                   \
-    git                        \
-    vim                        \
-    ripgrep                    \
+sudo apt install                \
+    curl                        \
+    htop                        \
+    xsel                        \
+    keychain                    \
+    git                         \
+    vim                         \
+    ripgrep                     \
     gettext -y
 
 # Normal Person Applications
-sudo apt install               \
+sudo apt install                \
     npm -y
 
 # Update to latest NPM
