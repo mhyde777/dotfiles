3abf49e7adf7effd5f1eb56ed43a64e87890a342
Mason Hyde <mason_hyde@uri.edu>
2025-09-07 14:45:00 -0400

    Made changes from OG (Mack's) to fit my needs

--- files/config/nvim/init.lua
+++ files/config/nvim/init.lua
@@ -1,3 +1,21 @@
+-- Load your existing config
 require("user.core")
 require("user.lazy")
+
+-- Check if this is an SSH session
+local is_ssh = vim.env.SSH_CONNECTION ~= nil
+
+if is_ssh then
+  -- Optional banner on remote start
+  print("🌐 SSH SESSION: Remote editing on mason-ms-7e06")
+
+  -- Use a distinct colorscheme
+  vim.cmd("colorscheme habamax") -- choose any theme that looks different
+
+  -- Optional: tweak background for visual signal
+  vim.cmd("highlight Normal guibg=#1c1c1c")
+
+  -- Optional: update statusline as a signal
+  vim.opt.statusline = "%#StatusLine# SSH 📡 [%f] %y %m %= %l:%c "
+end
 
